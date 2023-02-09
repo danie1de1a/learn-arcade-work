c4d6d13f59dd3c9ea8ba3efbe1ab6986f744d144
danie1de1a <daniel.fuentesanchez@edu.uah.es>
2023-02-09 15:59:55 +0100

    Lab 01 completed

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,1 +1,6 @@
-"""This is my first program!"""
+print('This is my first program!')
+print('quesooooo')
+print('me gustan las croquetas')
+a=1
+b=-6
+print(str(a)+'x'+str(b)+'+'+str(a)+'='+str(a*b+a))
